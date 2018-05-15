1221db125ce76c013675ef786daaf03251d3fafc
Anna <AnnFedosova@yandex.ru>
2018-05-15 19:52:46 +0300

    Настроена БД и деплой на сервер WildFly

---
+++ server/src/main/java/dBService/DBException.java
@@ -0,0 +1,11 @@
+package dBService;
+
+public class DBException extends Exception {
+    public DBException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public DBException(String message)  {
+        super(message);
+    }
+}
