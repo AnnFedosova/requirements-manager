d1ce2dd471678bb9623d40d1d852246a79a9bd1d
Anna <AnnFedosova@yandex.ru>
2018-06-15 20:22:56 +0300

    DTO на клиенте

---
+++ web/src/main/java/api/ProjectAPI.java
@@ -0,0 +1,38 @@
+package api;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import dto.ProjectDTO;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+public class ProjectAPI {
+    private final static String URL = ServerConnection.API_URL + "projects/";
+
+    public static List<ProjectDTO> getProjectsList() throws Exception {
+        String json = JSONHelper.getJson(URL + "getProjectsList");
+        return new Gson().fromJson(json, new TypeToken<List<ProjectDTO>>(){}.getType());
+    }
+
+    public static ProjectDTO getProject(long projectId) throws Exception {
+        String json = JSONHelper.getJson(URL + "getProject/" + projectId);
+        return new Gson().fromJson(json, new TypeToken<ProjectDTO>(){}.getType());
+    }
+
+    /*
+    public static List<ProjectPosition> getProjectPositions(long projectId) throws Exception {
+        String json = JSONHelper.getJson(URL + "getProjectPositions/" + projectId);
+        return new Gson().fromJson(json, new TypeToken<List<ProjectPosition>>(){}.getType());
+    }
+    */
+
+    public static Response editProject(ProjectDTO project) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(URL + "editProject");
+        Invocation.Builder builder = target.request();
+        return builder.post(Entity.entity(project, MediaType.APPLICATION_JSON));
+    }
+}
---
+++ web/src/main/java/servlets/ProjectServlet.java
@@ -0,0 +1,59 @@
+package servlets;
+
+import api.ProjectAPI;
+import api.UserAPI;
+import dto.ProjectDTO;
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "Project", urlPatterns = "/project")
+@ServletSecurity(@HttpConstraint(rolesAllowed = {"admin", "user"}))
+public class ProjectServlet extends HttpServlet {
+    public ProjectServlet() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        String id = request.getParameter("id");
+
+        Map<String, Object> pageVariables = null;
+        try {
+            pageVariables = createPageVariablesMap(request, Long.parseLong(id));
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println(PageGenerator.getInstance().getPage("project/project.html", pageVariables));
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Error!  " + HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    private Map<String, Object> createPageVariablesMap(HttpServletRequest request, long id) throws Exception {
+        Map<String, Object> pageVariables = new HashMap<>();
+        ProjectDTO project = ProjectAPI.getProject(id);
+        //List<ProjectPosition> positions = ProjectAPI.getProjectPositions(id);
+        //List<Request> requests = RequestAPI.getRequestsList(id);
+        Principal user = request.getUserPrincipal();
+//todo дописать, когда будет готово userProjectRoleDTO
+        pageVariables.put("isAdmin", UserAPI.isAdmin(user.getName()));
+        pageVariables.put("project", project);
+        //pageVariables.put("positions", positions);
+        //pageVariables.put("requests", requests);
+
+        return pageVariables;
+    }
+
+
+}
